ff5a35265b7fa428ccd16220492b93462f4f13af
Gianluigi <gianluigi.capriello@gmail.com>
2025-03-19 16:22:38 +0100

    commit nuovo

--- src/main/java/org/example/Records/Attore.java
+++ src/main/java/org/example/Records/Attore.java
@@ -3,6 +3,10 @@ package org.example.Records;
 public record Attore(
         long actorId,
         String firstName,
-        String lastName
+        String lastName,
+        int age
 ) {
+    public void nomeComple(){
+        System.out.println(firstName() + " " + lastName());
+    }
 }
